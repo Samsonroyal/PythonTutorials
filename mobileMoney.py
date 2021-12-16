e003c3fe6b72ad41b1a0b41e0d42af7352923721
Samson Royal <werefast2000@gmail.com>
2021-12-16 23:57:42 +0300

    ADDED NEW FILE

---
+++ mobileMoney.py
@@ -0,0 +1,5 @@
+list = ["Check Balance", "Send Money", "Withraw Money", "Pay Bill", "Payments","My Account"]
+print(list) 
+  
+print("After using the mapping technique: ")
+print('\n'.join(map(str, list)))
